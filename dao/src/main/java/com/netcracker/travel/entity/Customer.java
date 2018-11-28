de5dea1661963c1924c9b446a77d786cfbf712f2
UNiqkue <nik.nikita4@yandex.ru>
2018-11-28 17:30:28 +0300

    Change realization in service, console and dao impl

---
+++ dao/src/main/java/com/netcracker/travel/entity/Customer.java
@@ -0,0 +1,96 @@
+package com.netcracker.travel.entity;
+
+import com.netcracker.travel.entity.enumeration.Role;
+
+import java.sql.Date;
+import java.util.List;
+import java.util.Objects;
+
+public class Customer extends User {
+
+    private String phoneNumber;
+    private Date dateOfBirth;
+    private String cardNumber;
+    private String passportInfo;
+    private List<Tour> listOfTours;
+
+    public Customer(){}
+
+    @Override
+    public void setRole(Role role) {
+        super.setRole(Role.CUSTOMER);
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getPassportInfo() {
+        return passportInfo;
+    }
+
+    public void setPassportInfo(String passportInfo) {
+        this.passportInfo = passportInfo;
+    }
+
+    public List<Tour> getListOfTours() {        return listOfTours;    }
+
+    public void setListOfTours(List<Tour> listOfTours) {
+        this.listOfTours = listOfTours;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(getId(), customer.getId());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id='" + getId() + '\'' +
+                ", firstName='" + getFirstName() + '\'' +
+                ", lastName='" + getLastName() + '\'' +
+                ", username='" + getUsername() + '\'' +
+                ", password='" + getPassword() + '\'' +
+                ", email='" + getEmail() + '\'' +
+                ", activationCode='" + getActivationCode() + '\'' +
+                ", role=" + getRole() +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", cardNumber='" + cardNumber + '\'' +
+                ", passportInfo='" + passportInfo + '\'' +
+                '}';
+    }
+
+
+}
