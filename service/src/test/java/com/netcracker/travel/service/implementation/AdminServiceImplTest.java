c1adcac5ff3e3c2985f6333794bf5d7eecb61285
UNiqkue <nik.nikita4@yandex.ru>
2018-12-03 00:27:53 +0300

    Tests

--- service/src/test/java/com/netcracker/travel/service/implementation/AdminServiceImplTest.java
+++ service/src/test/java/com/netcracker/travel/service/implementation/AdminServiceImplTest.java
@@ -1,4 +1,40 @@
 package com.netcracker.travel.service.implementation;
 
+import com.netcracker.travel.service.interfaces.AbstractService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class AdminServiceImplTest {
+  /*  protected static final Logger LOG_R = Logger
+            .getLogger(AdminServiceImplTest.class);*/
+
+    @InjectMocks
+    private AdminServiceImpl adminService;
+
+    @Mock
+    private AbstractService abstractService;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        adminService = new AdminServiceImpl();
+    }
+
+    @Test
+    public void verifyPhoneNumberTest(){
+    }
+
+    @After
+    public void tearDown(){
+        adminService = null;
+    }
+
+
 }
--- service/src/test/java/com/netcracker/travel/service/implementation/CustomerServiceImplTest.java
+++ service/src/test/java/com/netcracker/travel/service/implementation/CustomerServiceImplTest.java
@@ -1,4 +1,52 @@
 package com.netcracker.travel.service.implementation;
 
+import com.netcracker.travel.dto.CustomerDto;
+import com.netcracker.travel.dto.RegistrationRequestDto;
+import com.netcracker.travel.exception.PhoneNumberException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CustomerServiceImplTest {
+
+    @InjectMocks
+    private CustomerServiceImpl customerService;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        customerService = new CustomerServiceImpl();
+    }
+
+    @Test
+    public void verifyPhoneNumberTest() throws PhoneNumberException, Exception {
+        PhoneNumberException exception = new PhoneNumberException();
+        //customerService.verifyPhoneNumber();
+        doThrow(exception).when(customerService).verifyPhoneNumber("1234567890");
+    }
+
+    @Test
+    public void registrationTest(){
+        RegistrationRequestDto registrationRequestDto = new RegistrationRequestDto();
+        CustomerDto customerDto = new CustomerDto();
+        customerDto.setUsername("qwerty");
+        customerDto.setPassword("1234567");
+        registrationRequestDto.setUsername("qwerty");
+        registrationRequestDto.setPassword("1234567");
+        when(customerService.registration(registrationRequestDto)).thenReturn(customerDto);
+
+    }
+
+    @After
+    public void tearDown(){
+        customerService = null;
+    }
 }
