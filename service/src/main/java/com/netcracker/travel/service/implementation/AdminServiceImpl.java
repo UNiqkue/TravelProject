2e17c1e92b2ebf1c1d09d2b13c602120611a123e
UNiqkue <nik.nikita4@yandex.ru>
2018-12-05 00:43:19 +0300

    Add @Autowired and resolve

--- service/src/main/java/com/netcracker/travel/service/implementation/AdminServiceImpl.java
+++ service/src/main/java/com/netcracker/travel/service/implementation/AdminServiceImpl.java
@@ -6,9 +6,13 @@ import com.netcracker.travel.dao.implementation.AdminDaoImpl;
 import com.netcracker.travel.dao.implementation.TourDaoImpl;
 import com.netcracker.travel.dto.AdminDto;
 import com.netcracker.travel.dto.TourDto;
+import com.netcracker.travel.entity.Tour;
+import com.netcracker.travel.repository.TourRepository;
 import com.netcracker.travel.service.interfaces.AbstractService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,6 +21,9 @@ public class AdminServiceImpl implements AbstractService<AdminDto> {
 
     private AdminDaoImpl adminDao = AdminDaoImpl.getInstance();
     private TourDaoImpl tourDao = TourDaoImpl.getInstance();
+
+    @Autowired
+    private TourRepository tourRepository;
 
     private AdminConverter adminConverter = new AdminConverter();
     private TourConverter tourConverter = new TourConverter();
@@ -36,10 +43,12 @@ public class AdminServiceImpl implements AbstractService<AdminDto> {
     }
 
     public List<TourDto> watchTours() {
-        return tourDao.getAll()
-                .stream()
-                .map(tour -> tourConverter.convert(tour))
-                .collect(Collectors.toList());
+        Iterable<Tour> tours = tourRepository.findAll();
+        List<TourDto> tourDtos = new ArrayList<>();
+        for (Tour tour : tours){
+            tourDtos.add(tourConverter.convert(tour));
+        }
+        return tourDtos;
     }
 
 
