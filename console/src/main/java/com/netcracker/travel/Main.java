81a663cbb2b24c9d146d203702379e78187eaa8c
UNiqkue <nik.nikita4@yandex.ru>
2018-12-05 00:42:27 +0300

    Add annotations @Scan, @Enable

--- console/src/main/java/com/netcracker/travel/Main.java
+++ console/src/main/java/com/netcracker/travel/Main.java
@@ -5,8 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.netcracker.travel")
+@EntityScan("com.netcracker.travel.entity")
+@EnableJpaRepositories("com.netcracker.travel.repository")
 public class Main implements CommandLineRunner {
     private static Logger LOG = LoggerFactory.getLogger(Main.class);
 
