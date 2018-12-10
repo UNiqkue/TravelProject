9d4f37a1380d1391bc1a2503e4bc27bb5dd87d3b
UNiqkue <nik.nikita4@yandex.ru>
2018-12-10 15:44:49 +0300

    Add controllers

--- console/src/main/java/com/netcracker/travel/controller/AuthenticationController.java
+++ console/src/main/java/com/netcracker/travel/controller/AuthenticationController.java
@@ -1,119 +1,19 @@
 package com.netcracker.travel.controller;
 
-import com.netcracker.travel.Menu;
 import com.netcracker.travel.dto.LoginRequestDto;
 import com.netcracker.travel.service.implementation.AuthenticationServiceImpl;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-@Controller
+@Api
+@RestController
+@RequestMapping("/login")
 public class AuthenticationController {
 
     @Autowired
     private AuthenticationServiceImpl authenticationService;
-    private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     private LoginRequestDto loginRequestDto;
-
-    public void login() {
-        boolean exit = false;
-        while (!exit) {
-            printWindow();
-            try {
-                int x = Integer.parseInt(reader.readLine());
-                switch (x) {
-                    case 1:
-                        requestCustomer();
-                        break;
-                    case 2:
-                        requestTravelAgent();
-                        break;
-                    case 3:
-                        requestAdmin();
-                        break;
-                    case 0:
-                        exit = true;
-                        break;
-                    default:
-                        Menu.printMesInput();
-                        break;
-                }
-            } catch (NumberFormatException e) {
-                System.out.println("Wrong input");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private void requestAdmin() {
-        try {
-            loginRequestDto = inputLoginRequest();
-            if (authenticationService.loginAdmin(loginRequestDto)) {
-                Menu.adminConsole();
-            } else {
-                printRequestError();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void requestTravelAgent() {
-        try {
-            loginRequestDto = inputLoginRequest();
-            if (authenticationService.loginTravelAgent(loginRequestDto)) {
-                Menu.travelAgentConsole(loginRequestDto.getUsername());
-            } else {
-                printRequestError();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void requestCustomer() {
-        try {
-            loginRequestDto = inputLoginRequest();
-            if (authenticationService.loginCustomer(loginRequestDto)) {
-                Menu.customerConsole(loginRequestDto.getUsername());
-            } else {
-                printRequestError();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private String inputUsername() throws IOException {
-        System.out.println("Please, input the username:");
-        String username = reader.readLine();
-        return username;
-    }
-
-    private String inputPassword() throws IOException {
-        System.out.println("Please, input the password:");
-        String password = reader.readLine();
-        return password;
-    }
-
-    private LoginRequestDto inputLoginRequest() throws IOException {
-        String username = inputUsername();
-        String password = inputPassword();
-        loginRequestDto = new LoginRequestDto(username, password);
-        return loginRequestDto;
-    }
-
-    private void printWindow() {
-        System.out.println("You registered by? \n 1. Customer \n 2. TravelAgent \n 3. Admin \n 0. Exit");
-    }
-
-    private void printRequestError() {
-        System.out.println("You input not corrected password");
-    }
-
 
 }
---
+++ console/src/main/java/com/netcracker/travel/controller/TourController.java
@@ -0,0 +1,12 @@
+package com.netcracker.travel.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api
+@RestController
+@RequestMapping("/tours")
+public class TourController {
+
+}
